5a98b80ba80066b45891bf9602fe95e110308792
hebel1992 <bartosz.hebrycz@gmail.com>
2019-10-21 14:44:49 +0200

    commit

--- src/main/java/pl/coderslab/controller/Admin/ManageExercises/DeleteExercise.java
+++ src/main/java/pl/coderslab/controller/Admin/ManageExercises/DeleteExercise.java
@@ -1,5 +1,7 @@
 package pl.coderslab.controller.Admin.ManageExercises;
 
+import pl.coderslab.dao.ExerciseDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +12,26 @@ import java.io.IOException;
 @WebServlet("/deleteExercise")
 public class DeleteExercise extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String param = request.getParameter("confirm");
+        int id = Integer.parseInt(request.getParameter("id"));
 
+        if (param != null) {
+            ExerciseDao exerciseDao = new ExerciseDao();
+            exerciseDao.delete(id);
+            response.sendRedirect("/displayExercises");
+        } else {
+            response.sendRedirect("/displayExercises");
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
 
+        ExerciseDao exerciseDao = new ExerciseDao();
+
+        request.setAttribute("exercise", exerciseDao.read(id));
+
+        getServletContext().getRequestDispatcher("/Admin/deleteExercise.jsp")
+                .forward(request, response);
     }
 }
--- src/main/java/pl/coderslab/controller/Admin/ManageExercises/EditExercise.java
+++ src/main/java/pl/coderslab/controller/Admin/ManageExercises/EditExercise.java
@@ -1,5 +1,8 @@
 package pl.coderslab.controller.Admin.ManageExercises;
 
+import pl.coderslab.dao.ExerciseDao;
+import pl.coderslab.models.Exercise;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +13,27 @@ import java.io.IOException;
 @WebServlet("/editExercise")
 public class EditExercise extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String newTitle = request.getParameter("newTitle");
+        String newDescription = request.getParameter("newDescription");
 
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        ExerciseDao exerciseDao = new ExerciseDao();
+        Exercise exercise = new Exercise(newTitle, newDescription);
+        exercise.setId(id);
+        exerciseDao.update(exercise);
+
+        response.sendRedirect("/displayExercises");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
 
+        ExerciseDao exerciseDao = new ExerciseDao();
+
+        request.setAttribute("exercise", exerciseDao.read(id));
+
+        getServletContext().getRequestDispatcher("/Admin/editExercise.jsp")
+                .forward(request, response);
     }
 }
